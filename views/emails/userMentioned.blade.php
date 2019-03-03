4f568a3f073973f875e23687b7b63d5940f17520
Robert Korulczyk <robert@korulczyk.pl>
2019-03-03 18:25:20 +0100

    Translate `userMentioned.blade.php` to Polish

--- views/emails/userMentioned.blade.php
+++ views/emails/userMentioned.blade.php
@@ -1,6 +1,6 @@
-Hey {!! $user->username !!}!
+Witaj {!! $user->username !!}!
 
-{!! $blueprint->post->user->username !!} mentioned you in a post in {!! $blueprint->post->discussion->title !!}.
+{!! $blueprint->post->user->username !!} wsponiał Ciebie w poście w {!! $blueprint->post->discussion->title !!}.
 
 {!! app()->url() !!}/d/{!! $blueprint->post->discussion_id !!}/{!! $blueprint->post->number !!}
 
